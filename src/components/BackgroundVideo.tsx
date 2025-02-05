56f48113ba1760689354cc23ceed5c7899d6e87f
wali  ahmad <waliahmadfiles@gmail.com>
2025-02-05 23:51:35 +0500

    update design

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -4,7 +4,13 @@ import React from "react";
 const BackgroundVideo = () => {
   return (
     <div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
-      <video autoPlay loop muted className="w-full h-full object-cover">
+      <video
+        autoPlay
+        playsInline
+        loop
+        muted
+        className="w-full h-full object-cover"
+      >
         <source src="/myvideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
