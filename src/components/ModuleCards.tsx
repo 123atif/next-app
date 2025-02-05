ca797c87e6a4c3d8646dc7b5073ba0bedb7ff57a
wali  ahmad <waliahmadfiles@gmail.com>
2025-02-05 18:05:24 +0500

    update section

--- src/components/ModuleCards.tsx
+++ src/components/ModuleCards.tsx
@@ -7,7 +7,7 @@ const ModuleCards = ({ playerCards, teamCards }) => {
   return (
     <div className="max-w-4xl mx-auto p-4 md:p-8">
       <Title
-        text="Glorizon Player NFT Cards"
+        text="Glorizen Player NFT Cards"
         className="mt-3 p-4 text-center"
       />
       <AnimatePresence mode="wait">
@@ -35,28 +35,23 @@ const ModuleCards = ({ playerCards, teamCards }) => {
           ))}
         </motion.div>
       </AnimatePresence>
-      <Title text="Glorizon Team NFT Cards" className="mt-3 p-4 text-center" />
+      <Title text="Glorizen Team NFT Cards" className="mt-3 p-4 text-center" />
       <AnimatePresence mode="wait">
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 20 }}
           transition={{ duration: 0.5 }}
-          className="flex flex-wrap justify-center gap-4 md:gap-8"
+          className=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6"
         >
           {teamCards.map((img, index) => (
             <motion.div
               key={index}
               whileHover={{ scale: 1.05 }}
               transition={{ type: "spring", stiffness: 300 }}
-              className="relative w-[200px] h-[250px] md:w-[250px] md:h-[300px] p-2"
+              className="relative"
             >
-              <Image
-                src={img}
-                alt={`Team Card ${index + 1}`}
-                fill
-                className="object-contain hover:z-10"
-              />
+              <Image src={img} alt={`Team Card ${index + 1}`} />
             </motion.div>
           ))}
         </motion.div>
